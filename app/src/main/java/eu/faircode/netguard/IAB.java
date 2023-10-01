00e0041e937fb4c17dc5592f2ab937cb61d19b70
Nobody <17956512+n0bodysec@users.noreply.github.com>
2023-10-01 11:47:39 -0300

    refactor: patch IAB for debugging purposes

--- app/src/main/java/eu/faircode/netguard/IAB.java
+++ app/src/main/java/eu/faircode/netguard/IAB.java
@@ -119,8 +119,9 @@ public class IAB implements ServiceConnection {
     public void updatePurchases() throws RemoteException {
         // Get purchases
         List<String> skus = new ArrayList<>();
-        skus.addAll(getPurchases("inapp"));
-        skus.addAll(getPurchases("subs"));
+        // skus.addAll(getPurchases("inapp"));
+        // skus.addAll(getPurchases("subs"));
+        skus.add(ActivityPro.SKU_PRO1);
 
         SharedPreferences prefs = context.getSharedPreferences("IAB", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
@@ -137,7 +138,7 @@ public class IAB implements ServiceConnection {
     }
 
     public boolean isPurchased(String sku, String type) throws RemoteException {
-        return getPurchases(type).contains(sku);
+        return true;
     }
 
     public List<String> getPurchases(String type) throws RemoteException {
@@ -176,41 +177,11 @@ public class IAB implements ServiceConnection {
     }
 
     public static boolean isPurchased(String sku, Context context) {
-        try {
-            if (Util.isDebuggable(context)) {
-                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-                return !prefs.getBoolean("debug_iab", false);
-            }
-
-            SharedPreferences prefs = context.getSharedPreferences("IAB", Context.MODE_PRIVATE);
-            if (ActivityPro.SKU_SUPPORT1.equals(sku) || ActivityPro.SKU_SUPPORT2.equals(sku))
-                return prefs.getBoolean(sku, false);
-
-            return (prefs.getBoolean(sku, false) ||
-                    prefs.getBoolean(ActivityPro.SKU_PRO1, false) ||
-                    prefs.getBoolean(ActivityPro.SKU_SUPPORT1, false) ||
-                    prefs.getBoolean(ActivityPro.SKU_SUPPORT2, false) ||
-                    prefs.getBoolean(ActivityPro.SKU_DONATION, false));
-        } catch (SecurityException ignored) {
-            return false;
-        }
+        return true;
     }
 
     public static boolean isPurchasedAny(Context context) {
-        try {
-            if (Util.isDebuggable(context)) {
-                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-                return !(prefs.getBoolean("debug_iab", false));
-            }
-
-            SharedPreferences prefs = context.getSharedPreferences("IAB", Context.MODE_PRIVATE);
-            for (String key : prefs.getAll().keySet())
-                if (prefs.getBoolean(key, false))
-                    return true;
-            return false;
-        } catch (SecurityException ignored) {
-            return false;
-        }
+        return true;
     }
 
     public static String getResult(int responseCode) {
